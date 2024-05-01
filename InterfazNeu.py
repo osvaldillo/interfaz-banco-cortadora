cb45010bc40648b73a30f594f6f934c75aab2a12
Osvaldo Alférez <osvaldoalferez77@gmail.com>
2024-04-30 21:24:45 -0600

    Version fusionada de Josué y Osvaldo. Sensores de final de carrera añadidos en el código del microcontrolador, y lógica mejorada. Interfaz con botón de conexion a microcontrolador añadido.

--- InterfazNeu.py
+++ InterfazNeu.py
@@ -2,7 +2,8 @@ import sys
 import cv2 
 from PyQt5 import uic
 from PyQt5.QtWidgets import * 
-from PyQt5.QtGui import QPixmap 
+from PyQt5.QtGui import QPixmap
+import serial
 
 class Window(QMainWindow): 
     def __init__(self):
@@ -10,7 +11,8 @@ class Window(QMainWindow):
         uic.loadUi("ProyectoNeu.ui",self) 
         
         self.setWindowTitle("Pneumatic Interface")
-        self.Start() 
+        self.Start()
+        self.setConnection()
         self.setStyleSheet('background-color: #55557f')  
         
         # self.img_cil = QPixmap('CilindroSF.png')  
@@ -21,7 +23,15 @@ class Window(QMainWindow):
         self.label_4.setStyleSheet('border-image : url(CilindroSF1.png);')
         self.Carrot.setStyleSheet('border-image : url(Carrot.png);')
         self.Cucumber.setStyleSheet('border-image : url(Cucumber.png);')
-        self.Ban.setStyleSheet('border-image : url(Ban.png);') 
+        self.Ban.setStyleSheet('border-image : url(Ban.png);')
+
+        self.AdvanceA.clicked.connect(self.sendData)
+        self.AdvanceB.clicked.connect(self.sendData)
+        self.RecoilA.clicked.connect(self.sendData)
+        self.RecoilB.clicked.connect(self.sendData)
+        self.Auto.clicked.connect(self.sendData)
+        self.connectionButton.clicked.connect(self.setConnection)
+        self.horizontalSlider.valueChanged.connect(self.sendData)
         
         self.GotoInstruct.setText("Instructions")
         self.GotoControls.setText("Controls")
@@ -40,6 +50,60 @@ class Window(QMainWindow):
     def Instruction(self): 
         self.stackedWidget.setCurrentWidget(self.Instructions) 
         self.GotoControls.clicked.connect(self.Control)
+
+    def setConnection(self, bt='Connect'):
+        try: btnTxt = self.sender().text()
+        except: btnTxt = bt
+        #Conectar
+        if btnTxt == "Connect":
+            ports = [f'COM{i}' for i in range(4, 12)]
+
+            for port in ports:
+                with serial.Serial() as self.board:
+                    self.board.port = port
+                    try:
+                        #self.placa.close()
+                        self.board = serial.Serial(port, 9600)
+                        self.conected = True
+                        self.connectionButton.setText("Disconnect")
+                        self.connectionStateLabel.setText(f'Connected on {self.board.port}')
+                        #print(self.cone)
+                        break
+                    except serial.serialutil.SerialException:
+                        continue
+            else:
+                print("Port nor found")
+                self.connectionStateLabel.setText(f'Port not found.')
+                self.conected = False
+        #Desconectar
+        else:
+            ports = [f'COM{i}' for i in range(4, 12)]
+            for port in ports:
+                with serial.Serial() as board:
+                    board.port = port
+                    try:
+                        board.close()
+                        self.conected = False
+                        self.connectionButton.setText("Connect")
+                        self.connectionStateLabel.setText(f'Disconnected')
+                        #print(self.conectado)
+                        break
+                    except serial.serialutil.SerialException:
+                        continue
+            else:
+                #print("No hay nada conectado")
+                self.labelConexion.setText(f'Port not found.')
+                self.conected = False
+
+    def sendData(self):
+        if self.conected:
+            if self.sender() == self.AdvanceA: send = 'A+'
+            elif self.sender() == self.AdvanceB: send = 'B+'
+            elif self.sender() == self.RecoilA: send = 'A-'
+            elif self.sender() == self.RecoilB: send = 'B-'
+            elif self.sender() == self.Auto: send = 'Start secuence'
+            elif self.sender() == self.horizontalSlider: send = f'flow({self.horizontalSlider.value()})'
+            self.board.write(f'{send},'.encode())
             
         
 app=QApplication(sys.argv)
--- codigo_placa_interfaz/codigo_placa_interfaz.ino
+++ codigo_placa_interfaz/codigo_placa_interfaz.ino
@@ -1,61 +1,63 @@
 String data;
-#define cilindroEmpujador 1
-#define cilindroCortador 11
-#define retroceso 10
-#define sensor 27
+#define cilinderAplus 4
+#define cilinderAminus 3
+#define cilinderB 2
+#define sensorA0 12
+#define sensorA1 11
+#define sensorB1 10
+
 int i;
 
 void setup() {
     Serial.begin(9600);
-    pinMode(cilindroEmpujador, OUTPUT);
-    pinMode(cilindroCortador, OUTPUT);
-    pinMode(retroceso, OUTPUT);
-    pinMode(sensor, INPUT);
+    pinMode(cilinderAplus, OUTPUT);
+    pinMode(cilinderAminus, OUTPUT);
+    pinMode(cilinderB, OUTPUT);
+    pinMode(sensorA0, INPUT);
+    pinMode(sensorA1, INPUT);
+    pinMode(sensorB1, INPUT);
 }
 
 void loop() {
     if (Serial.available() > 0) { 
         data = Serial.readStringUntil(','); 
-        if(data == "EC"){
-          digitalWrite(cilindroEmpujador, HIGH);
+        if(data == "A+"){
+          digitalWrite(cilinderAplus, HIGH);
+          delay(1000);
+          digitalWrite(cilinderAplus, LOW);
         }
-        else if(data == "CC"){
-          digitalWrite(cilindroEmpujador, LOW);
+        else if(data == "A-"){
+          digitalWrite(cilinderAminus, HIGH);
+          delay(1000);
+          digitalWrite(cilinderAminus, LOW);
         }
-        else if(data == "EG"){
-          digitalWrite(cilindroCortador, HIGH);
+        else if(data == "B+"){
+          digitalWrite(cilinderB, HIGH);
         }
-        else if(data == "CG"){
-          digitalWrite(cilindroCortador, LOW);
+        else if(data == "B-"){
+          digitalWrite(cilinderB, LOW);
         }
-        else if(data == "SE"){
-          i = 0;
-          while( i< 10){
-            digitalWrite(cilindroCortador, HIGH);
-            delay(100);
-            digitalWrite(cilindroCortador, LOW);
-            delay(100);
-            i++;
+        else if(data == "Start secuence"){
+          while(!digitalRead(sensorA0) or digitalRead(sensorA1) or digitalRead(sensorB1)){
+            digitalWrite(cilinderAminus, HIGH);
+            digitalWrite(cilinderB, LOW);
           }
-        }
-        else if (data == "PN"){
-          i = 0;
-          while(!digitalRead(sensor)){
-            digitalWrite(cilindroEmpujador, HIGH);
-            delay(1000);Z
-            digitalWrite(cilindroEmpujador, LOW);
-            digitalWrite(cilindroCortador, HIGH);
-            delay(1000);
-            digitalWrite(cilindroCortador, LOW);
-            delay(1000);
-            //i++;
+          digitalWrite(cilinderAminus, LOW);
+          delay(500);
+          while(!digitalRead(sensorA1)){
+            digitalWrite(cilinderAplus, HIGH);
+            delay(100);
+            digitalWrite(cilinderAplus, LOW);
+            digitalWrite(cilinderB, HIGH);
+            delay(100);
+            digitalWrite(cilinderB, LOW);
+            delay(100);
           }
-          digitalWrite(retroceso, HIGH);
-          delay(5000);
-          digitalWrite(retroceso, LOW);
-          
-
-
+          while(!digitalRead(sensorA0)){
+            digitalWrite(cilinderAminus, HIGH);
+            delay(1000);
+            digitalWrite(cilinderAminus, LOW);
+          }
         }
     }
 }
